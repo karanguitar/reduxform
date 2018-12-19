ec42da8692d5d7aebaab307188ebf6fc30f6c532
Karan Dhanoya <kdhanoya94@gmail.com>
2018-12-19 10:37:18 +0000

    final without styling

--- src/App.js
+++ src/App.js
@@ -1,100 +1,96 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import {Field, reduxForm, FieldArray} from 'redux-form'
 
-class App extends Component {
-
-  renderField = ({ input, label, type, meta: { touched, error } }) => (
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}</label>
     <div>
-      <label>{label}</label>
-      <div>
-        <input {...input} type={type} placeholder={label} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <input {...input} type={type} placeholder={label} />
+      {touched && error && <span>{error}</span>}
     </div>
-  )
+  </div>
+)
 
-  renderHobbies = ({ fields, meta: { error } }) => (
-    <ul>
-      <li>
-        <button type="button" onClick={() => fields.push()}>
-          Add Exercise
-        </button>
+
+const renderDays = ({ fields, meta: { error, submitFailed } }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push({})}>
+        Add day
+      </button>
+      {submitFailed && error && <span>{error}</span>}
+    </li>
+    {fields.map((day, index) => (
+      <li key={index}>
+        <button
+          type="button"
+          title="Remove day"
+          onClick={() => fields.remove(index)}
+        />
+        <h4>day #{index + 1}</h4>
+        <Field
+          name={`${day}.Name`}
+          type="text"
+          component={renderField}
+          label="Day Name e.g. Leg Day 1"
+        />
+        <FieldArray name={`${day}.exercises`} component={renderExercise} />
       </li>
-      {fields.map((exercise, index) => (
-        <li key={index}>
-          <button
-            type="button"
-            title="Remove exercise"
-            onClick={() => fields.remove(index)}
-          />
-          <Field
-            name={exercise}
-            type="text"
-            component={this.renderField}
-            label={`Exercise #${index + 1}`}
-          />
-        </li>
-      ))}
-      {error && <li className="error">{error}</li>}
-    </ul>
-  )
+    ))}
+  </ul>
+)
 
-  renderDay = ({ fields, meta: { error, submitFailed } }) => (
-    <ul>
-      <li>
-        <button type="button" onClick={() => fields.push({})}>
-          Add Day
-        </button>
-        {submitFailed && error && <span>{error}</span>}
+const renderExercise = ({ fields, meta: { error } }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push()}>
+        Add Exercise
+      </button>
+    </li>
+    {fields.map((exercise, index) => (
+      
+      <li key={index}>
+        <button
+          type="button"
+          title="Remove exercise"
+          onClick={() => fields.remove(index)}
+        />
+        {console.log(exercise)}
+        <Field
+          name={exercise}
+          
+          type="text"
+          component={renderField}
+          label={`Exercise #${index + 1}`}
+        />
       </li>
-      {fields.map((day, index) => (
-        <li key={index}>
-          <button
-            type="button"
-            title="Remove Day"
-            onClick={() => fields.remove(index)}
-          />
-          <h4>Day #{index + 1}</h4>
-          <Field
-            name={`day.name.${index + 1}`}
-            type="text"
-            component={this.renderField}
-            label="Day Name"
-          />
-          <FieldArray name={`${day}.exercise`} component={this.renderHobbies} />
-        </li>
-      ))}
-    </ul>
-  )
+    ))}
+    {error && <li className="error">{error}</li>}
+  </ul>
+)
 
-  render() {
-
-    const { handleSubmit, pristine, reset, submitting } = this.props
-
-    return (
-      <div className="App">
-        <form onSubmit={handleSubmit}>
-
-          <FieldArray name="days" component={this.renderDay} />
-          <div>
-            <button type="submit" disabled={submitting}>
-              Submit
-            </button>
-            <button type="button" disabled={pristine || submitting} onClick={reset}>
-              Clear Values
-            </button>
-          </div>
-        </form>
+const App = props => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <FieldArray name="days" component={renderDays} />
+      <div>
+        <button type="submit" disabled={submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
       </div>
-    );
-  }
+    </form>
+  )
 }
 
 export default reduxForm({
   
-  form: 'sports',
+  form: 'contact',
   destroyOnUnmount: false
 })(App)
 
